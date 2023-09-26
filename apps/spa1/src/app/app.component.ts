77c6cd91f6f23d936ea05031a48f8b86a00ac95a
Pedro J. Molina <pjmolina@gmail.com>
2023-09-26 15:28:50 +0200

    ejemplo de trackny en bucles

--- apps/spa1/src/app/app.component.ts
+++ apps/spa1/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Pais {
+  name: string;
+  id: number;
+}
 @Component({
   selector: 'org-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spa1';
+
+  paises: Pais[] = [
+    { id: 10, name: 'España' },
+    { id: 20, name: 'Francia' },
+    { id: 30, name: 'Italia' },
+    { id: 40, name: 'Portugal' },
+    { id: 50, name: 'Grecia' },
+    { id: 60, name: 'Alemania' },
+  ];
+
+  trackById(index: number, item: Pais): number {
+    return item.id;
+  }
 }
