2130788fbfe4bdce8ddec690b0aa42213e6393d1
Pedro J. Molina <pjmolina@gmail.com>
2023-09-26 15:08:02 +0200

    ChangeDetector example

--- apps/spa1/src/app/app.module.ts
+++ apps/spa1/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { TemperaturaComponent } from './temperatura/temperatura.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    //TemperaturaComponent
-  ],
+  declarations: [AppComponent, TemperaturaComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
--- apps/spa1/src/app/temperatura/temperatura.component.ts
+++ apps/spa1/src/app/temperatura/temperatura.component.ts
@@ -1,11 +1,46 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'org-temperatura',
   templateUrl: './temperatura.component.html',
   styleUrls: ['./temperatura.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class TemperaturaComponent {
   @Input() ciudad = '';
   @Input() temperatura = 0;
+
+  velodicadDelViento = 12;
+
+  numberOfTicks = 0;
+
+  constructor(private cd: ChangeDetectorRef) {
+    setInterval(() => {
+      this.cd.detach();
+      this.numberOfTicks++;
+      // require view to be updated
+      this.temperatura += this.numberOfTicks;
+
+      this.cd.reattach();
+      //this.cd.markForCheck();
+    }, 1000);
+  }
+
+  cambio(): void {
+    this.cd.detach();
+
+    this.ciudad = 'A';
+    this.temperatura = 20;
+    //... 1000 cambios mas
+
+    this.cd.reattach();
+    this.cd.markForCheck();
+
+    // modelo=estado    <-->       HTML
+  }
 }
